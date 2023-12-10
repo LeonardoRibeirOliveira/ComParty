d10af070b9e06140de8c5f69edb544422f5a057a
GabrielBGarcia <gabrielnpro@gmail.com>
2023-12-10 11:24:52 -0300

    fotos e texto

--- src/layouts/profile/components/Header/index.js
+++ src/layouts/profile/components/Header/index.js
@@ -67,7 +67,7 @@ function Header() {
           <Grid item>
             <SoftBox height="100%" mt={0.5} lineHeight={1}>
               <SoftTypography variant="h5" fontWeight="medium">
-                Alex Thompson
+                Jare
               </SoftTypography>
               <SoftTypography variant="button" color="text" fontWeight="medium">
                 CEO / Co-Founder
