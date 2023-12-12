688106bcba4267af7841bf69677d72206a04e6e5
thiago gomez <thiagogomez2906@hotmail.com>
2023-12-12 11:49:00 -0300

    chat box

--- src/layouts/room/components/Chat/chatbase.js
+++ src/layouts/room/components/Chat/chatbase.js
@@ -1,6 +1,12 @@
-export default inicitalChat = 
-    {
+const inicitalChat = [
+  {
+    senderId: "Jaré",
+    text: "Opaa, eae amigo tudo bom?"
+  },
+  {
       senderId: "Jaré",
       text: "Seja Bem-Vindo a ComParty, esta é a sua sala de chat, divirta-se e juízo hein :D"
-    }
-  ;
+  }
+];
+
+export default inicitalChat;
