74a6ff2e28f05ca6ab6ccfa186dd3c51caab491b
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2024-01-15 15:31:33 -0300

    Imagens no room-list

--- src/layouts/room-list/index.js
+++ src/layouts/room-list/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
 import SoftBox from "components/SoftBox";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import SoftTypography from "components/SoftTypography";
-import Button from '@mui/material/Button';
+import { Grid, Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
+
+import eletro from "./imgs/eletro.jpg";
+import mpb from "./imgs/mpb.png";
+import pop from "./imgs/pop-music.jpg";
+import rock from "./imgs/rock.jpg";
+import sertanejo from "./imgs/sertanejo.jpg";
 
 
 function RoomList() {
   const rooms = [
-    { id: 1, name: "Sertanejo", descricao: "Sertanejo mais bão que tem!", img: require(".//imgs/sertanejo.jpg").default },
-{ id: 2, name: "Rock", descricao: "Rockzao paulera.", img: require(".//imgs/rock.jpg").default },
-{ id: 3, name: "MPB", descricao: "Aquele MPB sem igual.", img: require(".//imgs/mpb.png").default },
-{ id: 4, name: "Pop", descricao: "Bora dancar, galera!", img: require(".//imgs/pop-music.jpg").default },
-{ id: 5, name: "Eletrofunk", descricao: "Batidão tuts tuts tuts tuts!", img: require(".//imgs/eletro.jpg").default },
+    { id: 1, name: "Sertanejo", descricao: "Sertanejo mais bão que tem!", img: <img width="300" height="300" src={sertanejo}/> },
+    { id: 2, name: "Rock", descricao: "Rockzao paulera.", img: <img width="300" height="300" src={rock}/>},
+    { id: 3, name: "MPB", descricao: "Aquele MPB sem igual.", img: <img width="300" height="300" src={mpb}/> },
+    { id: 4, name: "Pop", descricao: "Bora dancar, galera!", img: <img width="300" height="300" src={pop}/> },
+    { id: 5, name: "Eletrofunk", descricao: "Batidão tuts tuts tuts tuts!", img: <img width="300" height="300" src={eletro}/> },
     // Adicione mais salas conforme necessário
   ];
 
@@ -30,25 +35,21 @@ function RoomList() {
   
         <Grid container spacing={3} style={{ marginLeft: '-3px' }}> {/* Adiciona um espaçamento à esquerda */}
           {rooms.map((room) => (
-            <Grid item key={room.id} xs={20}>
-              <Button 
+            <Grid item key={room.id} xs={12} md={6} lg={4}>
+              <Button
+                component={Link}
+                to={`/room/${room.id}`}
                 variant="contained"
                 color="dark"
                 style={{ marginBottom: '16px', height: '100%', width: '100%'}}
               >
                 <Card>
-                  {/* Substitua 'url_da_sua_imagem' pela URL real da sua imagem */}
+                  {room.img}
                   <CardMedia
-                    component="img"
-                    height="100%"
-                    image={room.img}
                     alt={room.name}
                   />
                   <CardContent>
                     <Typography
-                      component={Link}
-                      to={`/room/${room.id}`}
-                      variant="button"
                       color="info"
                       fontWeight="medium"
                       textGradient
