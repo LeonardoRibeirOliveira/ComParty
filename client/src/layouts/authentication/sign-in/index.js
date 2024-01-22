7e09d860a0025b809164b74f12be3d2c65813972
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2024-01-22 03:49:24 -0300

    Adição do server websocket

---
+++ client/src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Switch from "@mui/material/Switch";
+
+// Soft UI Dashboard React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftInput from "components/SoftInput";
+import SoftButton from "components/SoftButton";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import curved9 from "assets/images/curved-images/curved14.jpg";
+
+function SignIn() {
+  const [rememberMe, setRememberMe] = useState(true);
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  return (
+    <CoverLayout
+      title="Bem vindo de volta!"
+      description="INSIRA SEU NOME PARA CONTINUAR"
+      image={curved9}
+    >
+      <SoftBox component="form" role="form">
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Nickname
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="email" placeholder="Nickname" />
+        </SoftBox>
+        <SoftBox mt={4} mb={1}>
+          <SoftButton variant="gradient" color="info" fullWidth>
+            sign in
+          </SoftButton>
+        </SoftBox>
+      </SoftBox>
+    </CoverLayout>
+  );
+}
+
+export default SignIn;
